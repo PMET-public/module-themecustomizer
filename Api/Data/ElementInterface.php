ac091560bdc6b60ebc268cb8953121bf47463e71
Jeffrey Britts <jbritts@magento.com>
2017-12-01 16:02:11 -0500

    publish via elements in the db vs config xml

--- Api/Data/ElementInterface.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace MagentoEse\ThemeCustomizer\Api\Data;
-interface ElementInterface
-{
-}
--- Controller/Adminhtml/Skin/Apply.php
+++ Controller/Adminhtml/Skin/Apply.php
@@ -1,11 +1,25 @@
 <?php
 namespace MagentoEse\ThemeCustomizer\Controller\Adminhtml\Skin;
 
+
+
 class Apply extends \Magento\Backend\App\Action
 {
     const ADMIN_RESOURCE = 'MagentoEse_ThemeCustomizer::skins';
     protected $skinDirectory ='/static/frontend/Magento/luma/en_US/MagentoEse_ThemeCustomizer/css/';
     protected $cssFilename = 'demo.css';
+    protected $context;
+    protected $elementRepository;
+
+
+    public function _construct(
+        \Magento\Backend\App\Action\Context $context,
+        \MagentoEse\ThemeCustomizer\Api\ElementRepositoryInterfaceFactory $elementRepository
+
+    ){
+        $this->elementRepository = $elementRepository;
+      // parent::__construct($context,$elementRepository);
+    }
 
     public function execute()
     {
@@ -48,65 +62,26 @@ class Apply extends \Magento\Backend\App\Action
 
     private function generateCssContent($model)
     {
-        //$helper = $this->_helperData;
-        //define configs
-        /*$top_bar_color = $model->getData('topbarcolor');
-        $primary_font_color = $helper->getPrimaryFontColor();
-        $primary_link_color = $helper->getPrimaryLinkColor();
-        $primary_link_hover_color = $helper->getPrimaryLinkHoverColor();
-        $primary_heading_color = $helper->getPrimaryHeadingColor();
-        $primary_price_color = $helper->getPrimaryPriceColor();
-        $background_color = $helper->getBackgroundColor();
-        $category_grid_background_color = $helper->getCategoryGridBackgroundColor();
-        $product_view_background_color = $helper->getProductViewBackgroundColor();
-        $block_content_background_color = $helper->getBlockContentBackgroundColor();
-        $nav_background_color = $helper->getNavBackgroundColor();
-        $nav_link_color = $helper->getNavLinkColor();
-        $nav_link_hover_color = $helper->getNavLinkHoverColor();
-        $nav_dropdown_background_color = $helper->getNavDropdownBackgroundColor();
-        $nav_dropdown_link_color = $helper->getNavDropdownLinkColor();
-        $nav_dropdown_link_hover_color = $helper->getNavDropdownLinkHoverColor();
-        $button_background_color = $helper->getButtonBackgroundColor();
-        $button_link_color = $helper->getButtonLinkColor();
-        $button_link_hover_color = $helper->getButtonLinkHoverColor();
-        //logo assets
-        $asset_logo = $helper->getAssetLogo();
-        $additional_css = $helper->getAdditionalCSS();*/
-        //future : switch statement based on param for design / css profile
-        //will support LUMA and default EE theme
-        //build content
+        $id = $this->getRequest()->getParam('object_id');
+        $skin = $this->_objectManager->create('MagentoEse\ThemeCustomizer\Model\ResourceModel\Skin\Collection');
+        $skinModel = $skin->load($id);
+        $elementData = $this->_objectManager->create('MagentoEse\ThemeCustomizer\Model\Element');
+        //$data1 = $data->create();
+        $elements = $elementData->load(1);
         $css_content = '/* THIS FILE IS AUTO-GENERATED, DO NOT MAKE MODIFICATIONS DIRECTLY */' . "\n";
-        $css_content .= '.page-header .panel.wrapper { background-color:' . $top_bar_color . ' !important;}' . "\n";
-        $css_content .= 'body { color:' . $primary_font_color . ' !important;}' . "\n";
-        $css_content .= 'a { color:' . $primary_link_color . '!important;}' . "\n";
-        $css_content .= 'a:hover { color:' . $primary_link_hover_color . '!important;}' . "\n";
-        $css_content .= '.no-touch .product-image:hover {border-color:'.$primary_link_hover_color. '!important}'."\n";
-        $css_content .= 'h1,h2,h3,h4,h5,h6,h7, .product-name .h1, .block-title h2, .block-title h3, .block-title strong, .footer .block-title, .footer address{ color:' . $primary_heading_color . '!important;}' . "\n";
-        $css_content .= '.price-box .price { color:' . $primary_price_color . '!important;}'. "\n";
-        $css_content .= 'body, .wrapper, .skip-link { background-color:' . $background_color . '!important;}' . "\n";
-        $css_content .= '.category-products{ background-color:' . $category_grid_background_color . '!important;}' . "\n";
-        $css_content .= '.product-view{ background-color:' . $product_view_background_color . '!important;}' . "\n";
-        $css_content .= '.block-content{ background-color:' . $block_content_background_color . '!important;}' . "\n";
-        $css_content .= '.nav-primary { background-color:' . $nav_background_color . '!important;}' . "\n";
-        $css_content .= '.nav-primary a { color:' . $nav_link_color . '!important;}' . "\n";
-        $css_content .= '.nav-primary a:hover { color:' . $nav_link_hover_color . '!important;}' . "\n";
-        $css_content .= '.nav-primary li.level0 ul  { background-color:' . $nav_dropdown_background_color . '!important;}' . "\n";
-        $css_content .= '.nav-primary li.level1 a  { color:' . $nav_dropdown_link_color . '!important;}' . "\n";
-        $css_content .= '.nav-primary li.level1 a:hover  { color:' . $nav_dropdown_link_hover_color . '!important;}' . "\n";
-        $css_content .= '.button, .cart-table .product-cart-actions .button, #co-shipping-method-form .buttons-set .button, .footer .button  { background-color:' . $button_background_color . '!important;}' . "\n";
-        $css_content .= 'a.button  { color:' . $button_link_color . '!important;}' . "\n";
-        $css_content .= 'a.button:hover  { color:' . $button_link_hover_color . '!important;}' . "\n";
-        $css_content .= '.input-text:focus {border-color:'.$button_background_color. '!important}'."\n";
-        $css_content .= '.slideshow-next:hover:before {border-color:transparent transparent transparent '.$button_background_color. '!important}'."\n";
-        $css_content .= '.slideshow-pager span.cycle-pager-active:before, .slideshow-pager span:hover:before { background-color:'.$button_background_color. '!important}'."\n";
-        //if($helper->getAssetLogo()){
-        //    $css_content .= '.logo img {display:none!important;}'."\n";
-       //     $css_content .= '.logo {background: url('.$asset_logo.') top left no-repeat!important; min-width:300px; min-height:90px; max-width:300px!important; max-height:90px!important;}'."\n";
-        //}
-        //misc fixes
-        //$css_content .= 'button.search-button { background:none repeat scroll 0 0 rgba(0, 0, 0, 0)!important;}' . "\n";
-        //$css_content .= '.minicart-actions .checkout-button { background-color:#3399cc!important; color:#fff;}' . "\n";
-        $css_content .= $model->getData('additional_css');
+        foreach ($elements->getCollection()->getData() as $element )
+        {
+            $inString = $element['css_code'];
+            $toFind = "$".$element['element_code'];
+            $replaceWith = $skinModel->getData()[0][$element['element_code']];
+            if($replaceWith != null){
+                $css_content.= str_replace($toFind,$replaceWith,$inString);
+            }
+            //$css_content.= str_replace($element['element_code'],$skinModel->getData())
+
+        }
+
+        $css_content .= $skinModel->getData()[0]['additional_css'];
         return $css_content;
     }
     public function _createCSSFile($contents)
