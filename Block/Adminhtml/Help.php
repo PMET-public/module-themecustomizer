9595fce84e715aeed1d7c4d504707d61e231e397
Jeff Britts <jbritts@magento.com>
2018-03-20 10:29:55 -0400

    added block for links

---
+++ Block/Adminhtml/Help.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jbritts
+ * Date: 3/19/18
+ * Time: 3:41 PM
+ */
+
+namespace MagentoEse\ThemeCustomizer\Block\Adminhtml;
+
+
+class Help extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+    {
+        parent::__construct($context);
+    }
+}
